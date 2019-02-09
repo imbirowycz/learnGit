1de786f0050ee2a3088b6283fbcfe97f9732a6c8
imbirowycz <a.imbirowycz@gmail.com>
2019-02-09 13:13:45 +0100

    add settings

---
+++ src/components/Settings.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Settings">
+    <h2>Settings</h2>
+    <p>Hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
